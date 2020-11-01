eb6549d79e09816d3b1c995b12336faab55a416d
Syangjie <3208989399@qq>
2020-11-01 22:59:40 +0800

    第2次-level2-杨杰-2020214980

---
+++ slow/src/Go.java
@@ -0,0 +1,17 @@
+class yes {
+    public static void main(String[] args) {
+        long mun_1 = 4, num2 = 3;
+        long l= mun_1;
+        System.out.print("为" + l*l);
+        System.out.println("************");
+        System.out.println("****JAVA****");
+        /*System.out.println("************");
+         */
+        //哈哈哈哈哈哈哈
+        //
+        for (int i = 1 ; i < 2; i++) {
+            System.out.println("i"+i);
+
+        }
+    }
+}
